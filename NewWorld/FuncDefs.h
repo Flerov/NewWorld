a6c8714749418d3e2ddfb1416f7e13dbd7091a86
zyphex <30928491+Flerov@users.noreply.github.com>
2025-03-14 00:15:48 +0100

    Add files via upload

---
+++ NewWorld/FuncDefs.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "main.h"
+
+NTSTATUS DriverDispatcher(PDEVICE_OBJECT pDeviceObject, PIRP pIrp);
+void DriverUnload(PDRIVER_OBJECT pDriverObject);
+NTSTATUS DeviceDispatcher(PDEVICE_CONTEXT pDeviceContext, PIRP pIrp);
+NTSTATUS SpyHookRemove(BOOL fReset, PDWORD pdCount);
+void SpyWriteReset(PSPY_PROTOCOL psp);
+void SpyHookCleanup(void);
+DWORD SpyHookExchange(void);
+BOOL SpyHookPause(BOOL fPause);
+NTSTATUS SpyHookWait(void);
+void SpyHookReset(void);
+LONG SpyHookRelease(void);
+NTSTATUS SpyDispatcher(PDEVICE_CONTEXT pDeviceContext,DWORD dCode,PVOID pInput,DWORD dInput,PVOID pOutput,DWORD dOutput,PDWORD pdInfo);
+BOOL SpyMemoryPageEntry(PVOID pVirtual, PSPY_PAGE_ENTRY pspe);
