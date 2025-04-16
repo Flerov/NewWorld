e72e1549eb9c1f9b6224374ca23902d1c65f53d1
zyphex <30928491+Flerov@users.noreply.github.com>
2025-04-16 20:45:02 +0200

    Update FuncDefs.h

--- NewWorld/FuncDefs.h
+++ NewWorld/FuncDefs.h
@@ -14,3 +14,4 @@ void SpyHookReset(void);
 LONG SpyHookRelease(void);
 NTSTATUS SpyDispatcher(PDEVICE_CONTEXT pDeviceContext,DWORD dCode,PVOID pInput,DWORD dInput,PVOID pOutput,DWORD dOutput,PDWORD pdInfo);
 BOOL SpyMemoryPageEntry(PVOID pVirtual, PSPY_PAGE_ENTRY pspe);
+VOID VirtToPhys(unsigned long long addr, PEPROCESS TargetProcess, unsigned long long cr3, BOOLEAN log);
